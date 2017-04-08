aaf4b0adb68718c77e7be16f1955951a4a98d2a9
aashreys <aashrey9sharma@gmail.com>
2017-04-08 14:31:17 +0530

    Removed annotations and add remove API

--- lib/src/main/java/com/aashreys/maestro/ViewModelStore.java
+++ lib/src/main/java/com/aashreys/maestro/ViewModelStore.java
@@ -1,12 +1,8 @@
 package com.aashreys.maestro;
 
-import com.sun.istack.internal.Nullable;
-
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
-
-import javax.inject.Inject;
 
 /**
  * Created by aashreys on 08/04/17.
@@ -16,7 +12,6 @@ public class ViewModelStore {
 
     private final Map<String, ViewModel> viewModelMap;
 
-    @Inject
     public ViewModelStore() {
         this.viewModelMap = Collections.synchronizedMap(new HashMap<String, ViewModel>());
     }
@@ -25,9 +20,12 @@ public class ViewModelStore {
         viewModelMap.put(viewId, model);
     }
 
-    @Nullable
     public ViewModel get(String viewId) {
         return viewModelMap.get(viewId);
+    }
+
+    public ViewModel remove(String viewId) {
+        return viewModelMap.remove(viewId);
     }
 
 }
